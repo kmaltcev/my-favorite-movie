604a36ddb6c8c397c94ce82167c969fb8980faa7
Tal <kotlertal22@gmail.com>
2021-07-21 16:47:54 +0300

    Solved broadcast reciever

---
+++ app/src/main/java/com/example/myfavoritemovie/app/dependency/NetworkChangeReceiver.kt
@@ -0,0 +1,46 @@
+package com.example.myfavoritemovie.app.dependency
+
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.widget.Toast
+
+const val NO_ACTIVE_NETWORK = 0
+const val CELLULAR_NETWORK = 1
+const val WIFI_NETWORK = 2
+
+class NetworkChangeReceiver: BroadcastReceiver() {
+    private var lastAvailable = 0
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val currNetworkStatus = checkNetwork(context)
+        when(currNetworkStatus) {
+            NO_ACTIVE_NETWORK ->
+                if (lastAvailable != NO_ACTIVE_NETWORK)
+                    Toast.makeText(context, "Network Unavailable", Toast.LENGTH_SHORT).show()
+            CELLULAR_NETWORK ->
+                if (lastAvailable != CELLULAR_NETWORK)
+                    Toast.makeText(context, "Cellular Network Available", Toast.LENGTH_SHORT).show()
+            WIFI_NETWORK ->
+                if (lastAvailable != WIFI_NETWORK)
+                    Toast.makeText(context, "Wifi Network Available", Toast.LENGTH_SHORT).show()
+        }
+        lastAvailable = currNetworkStatus
+    }
+
+    private fun checkNetwork(context: Context?): Int {
+        val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = cm.activeNetwork ?: return NO_ACTIVE_NETWORK
+        val actNw = cm.getNetworkCapabilities(networkCapabilities) ?: return NO_ACTIVE_NETWORK
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> WIFI_NETWORK
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> CELLULAR_NETWORK
+            else -> NO_ACTIVE_NETWORK
+        }
+    }
+}
+
+
+
--- app/src/main/java/com/example/myfavoritemovie/ui/MainActivity.kt
+++ app/src/main/java/com/example/myfavoritemovie/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myfavoritemovie.ui
 
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfavoritemovie.R
+import com.example.myfavoritemovie.app.dependency.NetworkChangeReceiver
 import com.example.myfavoritemovie.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -12,6 +14,9 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private val receiver = NetworkChangeReceiver()
+    private val filter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +34,15 @@ class MainActivity : AppCompatActivity() {
         if (auth.currentUser == null) {
             auth.signInAnonymously()
         }
+    }
+
+    override fun onResume() {
+        registerReceiver(receiver, filter)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        unregisterReceiver(receiver)
+        super.onPause()
     }
 }
